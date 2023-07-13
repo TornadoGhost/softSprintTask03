a9311093696fcab9b75a45721e8b018cbe00fe8c
tornadoghost <tornadoghost97@gmail.com>
2023-07-14 01:52:30 +0300

    rewrited

--- public/Controllers/operations.php
+++ public/Controllers/operations.php
@@ -1,29 +1,22 @@
 <?php
 include_once '../Models/UserModel.php';
 
-$action = $_POST['actions1'];
-
-if(!empty($action) && empty($_POST['checkboxId'])){
-    echo json_encode(['status' => false, 'error' => ['code' => 404, 'message' => 'Action selected but not user or users', 'info' => 'noUserIsAction']]);
-    die();
-}
-if(empty($action) && !empty($_POST['checkboxId'])) {
-    echo json_encode(['status' => false, 'error' => ['code' => 404, 'message' => 'User or users selected but not action', 'info' => 'noActionIsUser']]);
-    die();
-}
-
-if(!empty($_POST['actions1']) && !empty($_POST['checkboxId'])) {
-    if ($action === 'Delete') {
-        $ids = action('delete');
-        echo json_encode(['status' => true, 'error' => null, 'ids' => $ids, 'action' => 'delete']);
-    } elseif ($action === 'Set active') {
-        $ids = action('setActive');
-        echo json_encode(['status' => true, 'error' => null, 'ids' => $ids, 'action' => 'active']);
-    } else if ($action === 'Set not active') {
-        $ids = action('setNotActive');
-        echo json_encode(['status' => true, 'error' => null, 'ids' => $ids, 'action' => 'notActive']);
-    } else {
-        echo json_encode(['status' => false, 'error' => ['code' => 404, 'message' => 'Some error']]);
+dataProcessing(1);
+dataProcessing(2);
+function dataProcessing($buttonNumber) {
+    if($_POST['sendFrom'] === "button$buttonNumber" && !empty($_POST['checkboxId'])){
+        if($_POST['selectedAction'] === 'delete'){
+            $ids = action('delete');
+            echo json_encode(['status' => true, 'error' => null, 'ids' => $ids, 'action' => 'delete']);
+        }else if ($_POST['selectedAction'] === 'setActive'){
+            $ids = action('setActive');
+            echo json_encode(['status' => true, 'error' => null, 'ids' => $ids, 'action' => 'active']);
+        }else if ($_POST['selectedAction'] === 'setNotActive'){
+            $ids = action('setNotActive');
+            echo json_encode(['status' => true, 'error' => null, 'ids' => $ids, 'action' => 'notActive']);
+        } else {
+            echo json_encode(['status' => false, 'error' => ['code' => 404, 'message' => 'No actions selected']]);
+        }
     }
 }
 function action($method)
