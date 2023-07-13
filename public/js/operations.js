0b2e8de68865d0c0a932ee9ba58ff923bcfe2e1f
tornadoghost <tornadoghost97@gmail.com>
2023-07-14 01:52:16 +0300

    rewrited

--- public/js/operations.js
+++ public/js/operations.js
@@ -1,28 +1,110 @@
-$("#mainTable").on("submit", function () {
-    $.ajax({
-        url: '/Controllers/operations.php',
-        method: 'post',
-        data: $(this).serialize(),
-        success: function (data) {
-            if(!data){
+//actions for upper button OK
+
+operation(1);
+operation(2);
+function operation(number) {
+    $(`#sendButton${number}`).on("click", function (event) {
+        event.preventDefault();
+
+        let data = $("#mainTable").serializeArray();
+        const selectVal = $(`#actions${number}`).val();
+
+        const modal = $('#modalWindow');
+        modal.find('.modal-title').text("Warning");
+        modal.find('button[data-role=delete]').removeAttr('data-role');
+        modal.find('.btn-primary').text('Got it!');
+
+        const myObj = {};
+        data.forEach(function (element) {
+            myObj[element.name] = element.value;
+        });
+
+        if (((myObj[`actions${number}`] === `Delete${number}` || myObj[`actions${number}`] === `Set active${number}`|| myObj[`actions${number}`] === `Set not active${number}`) && myObj[`actions${number}`]) && !myObj['checkboxId[]']) {
+            modal.find('.modal-body-text').text("Please, choose user before making actions on them");
+            modal.modal('show');
+
+            return false
+        }
+
+        if (myObj[`actions${number}`] === '' && myObj['checkboxId[]']) {
+            modal.find('.modal-body-text').text("Please, choose action");
+            modal.modal('show');
+
+            return false
+        }
+
+        if (!selectVal) {
+            return false;
+        }
+
+        if (selectVal === `Delete${number}`) {
+            $('#confirmModal').modal('show');
+
+            $("#confirmDeleteButton").on('click', function () {
+                let formData = $("#mainTable").serialize();
+                formData += `&sendFrom=button${number}&selectedAction=delete`;
+                $.ajax({
+                    url: '/Controllers/operations.php',
+                    method: 'post',
+                    data: formData,
+                    success: function (data) {
+                        $('#message').html(data);
+                        if (!data) {
+                            return false;
+                        }
+                        data = JSON.parse(data);
+                        if (data.status) {
+                            data.ids.forEach(element => {
+                                $("tr.block__table-body-row input[value='" + element + "']")
+                                    .closest("tr.block__table-body-row")
+                                    .remove();
+                            });
+                        }
+                    }
+                });
+                $(`#confirmModal`).modal("hide");
                 return false;
-            }
-            data = JSON.parse(data);
-            if (data.status) {
-                if (data.action === 'delete') {
-                    data.ids.forEach(element => {
-                        $("tr.block__table-body-row input[value='" + element + "']")
-                            .closest("tr.block__table-body-row")
-                            .remove();
-                    });
-                } else if (data.action === 'active') {
+            });
+        }
+        console.log(selectVal);
+        if (selectVal === `Set active${number}`) {
+            let formData = $("#mainTable").serialize();
+            formData += `&sendFrom=button${number}&selectedAction=setActive`;
+            console.log(formData);
+            $.ajax({
+                url: '/Controllers/operations.php',
+                method: 'post',
+                data: formData,
+                success: function (data) {
+                    $('#message').html(data);
+                    if (!data) {
+                        return false;
+                    }
+                    data = JSON.parse(data);
                     data.ids.forEach(element => {
                         $("tr.block__table-body-row input[value='" + element + "']")
                             .closest("tr.block__table-body-row")
                             .find(".block__circle").removeClass('block__circle--gray')
                             .addClass('block__circle--green');
                     });
-                } else if (data.action === 'notActive') {
+                }
+            });
+            return false;
+        }
+
+        if (selectVal === `Set not active${number}`) {
+            let formData = $("#mainTable").serialize();
+            formData += `&sendFrom=button${number}&selectedAction=setNotActive`;
+            $.ajax({
+                url: '/Controllers/operations.php',
+                method: 'post',
+                data: formData,
+                success: function (data) {
+                    $('#message').html(data);
+                    if (!data) {
+                        return false;
+                    }
+                    data = JSON.parse(data);
                     data.ids.forEach(element => {
                         $("tr.block__table-body-row input[value='" + element + "']")
                             .closest("tr.block__table-body-row")
@@ -30,24 +112,8 @@ $("#mainTable").on("submit", function () {
                             .addClass('block__circle--gray');
                     });
                 }
-            }
-
-            const modal = $('#modalWindow');
-            modal.find('.modal-title').text("Warning");
-            modal.find('button[data-role=delete]').removeAttr('data-role');
-            modal.find('.btn-primary').text('Got it!');
-
-            if(!data.status  && typeof data.error.info !== "undefined") {
-                if(data.error.info === 'noUserIsAction') {
-                    modal.find('.modal-body-text').text("Please, choose user before making actions on them");
-                    modal.modal('show');
-                } else if (data.error.info === 'noActionIsUser') {
-                    modal.find('.modal-body-text').text("Please, choose action");
-                    modal.modal('show');
-                }
-            }
-
+            });
+            return false;
         }
     });
-    return false;
-});
+}
