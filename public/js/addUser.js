4346197e849eb49a2c4e757bc4316f5e0bf34c90
tornadoghost <tornadoghost97@gmail.com>
2023-07-21 02:43:14 +0300

    added setting input[name=status] on not checked

--- public/js/addUser.js
+++ public/js/addUser.js
@@ -2,6 +2,7 @@ $("button[data-role=add]").on('click', function () {
     $('#addUserForm .alert-danger').remove();
     const form = $('#addUserForm').closest('div');
     form.find('input').val('');
+    form.find('input[name=status]').prop('checked', false);
     form.find('input[type=checkbox]').removeAttr('checked');
     form.find('option').removeAttr('selected').first().prop('selected', 'selected');
     form.find('input[name=type]').removeAttr('value').attr('value', 'add');
