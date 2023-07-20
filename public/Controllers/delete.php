633432410a010342056f8674e3d97c03d9abe6e7
tornadoghost <tornadoghost97@gmail.com>
2023-07-21 00:05:22 +0300

    added check for users on existence

--- public/Controllers/delete.php
+++ public/Controllers/delete.php
@@ -2,12 +2,17 @@
 include_once '../Models/UserModel.php';
 include_once 'check.php';
 
-$save = new \public\Models\UserModel();
-$user_id = $_POST['id'];
-$result = $save->delete($user_id);
-$data = ['id' => $user_id];
-if($result) {
-    echo response($result, $data);
+$user = new \public\Models\UserModel();
+
+if(!empty(userExists($_POST['id']))){
+    $user_id = $_POST['id'];
+    $result = $user->delete($user_id);
+    if($result) {
+        $data = ['id' => $user_id];
+        echo response($result, $data);
+    } else {
+        echo error('Something went wrong.');
+    }
 } else {
-    echo json_encode(array('status' => false, 'error' => ['code' => 404, 'message' => 'User not found']));
+    echo error('User ' . $_POST['name'] . ' does not exist.');
 }
