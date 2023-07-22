c3e46b8817c08047589bffe108d9665f5a89a8d4
tornadoghost <tornadoghost97@gmail.com>
2023-07-22 23:59:18 +0300

    changed using #userNotExist modal window when user is not exit, now #modalWindow is using for it

--- public/js/delete.js
+++ public/js/delete.js
@@ -1,6 +1,3 @@
-$('#userNotExist button.btn').on('click', function () {
-    $('#userNotExist').addClass('is-hidden');
-})
 $(document).on('click', '.block__table-body-text button[data-role=delete]', function () {
     const modal = $('#modalWindow');
     const row = $(this).parents('.block__table-body-row');
@@ -10,9 +7,10 @@ $(document).on('click', '.block__table-body-text button[data-role=delete]', func
     modal.find('.modal-title').text("Delete Confirmation");
     modal.find('.modal-body-text').text("Are you sure you want to delete " + userName + "?");
     modal.find('.btn-primary').text('Delete').attr('data-role', 'delete');
-
     modal.modal('show');
-    modal.find('button[data-role=delete]').off('click').on('click', function () {
+
+    $(document).on('click', '#modalWindow button[data-role=delete]',  function () {
+        modal.find('.btn-primary').text('Close').removeAttr('data-role');
         const dataSend = {id: userId, name: userName};
         $.ajax({
             url: '/Controllers/delete.php',
@@ -21,17 +19,16 @@ $(document).on('click', '.block__table-body-text button[data-role=delete]', func
             success: function (data) {
                 data = JSON.parse(data);
                 if (!data.status) {
-                    const text = `<p>${data.error.message}</p>`;
-                    const textBody = $('#userNotExist .modal-body p');
-                    if (textBody.length === 0) {
-                        $('#userNotExist .modal-body').append(text);
-                    }
-                    $('#userNotExist').removeClass('is-hidden');
+                    console.log('tut');
+                    modal.find('.modal-body-text').text(`${data.error.message}`);
+                    modal.modal('show');
+                    $(document).off('click', '#modalWindow button[data-role=delete]');
+
                     return false;
                 }
                 row.remove();
                 modal.modal('hide');
-                modal.find('button[data-role=delete]').removeAttr('data-role');
+                $(document).off('click', '#modalWindow button[data-role=delete]');
             }
         });
 
